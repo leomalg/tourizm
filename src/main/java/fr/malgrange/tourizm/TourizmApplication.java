8dce190597a20e233e55125d97080a133d0d7a1c
capi <leo@malgrange.fr>
2019-03-03 22:57:08 +0100

    init app

---
+++ src/main/java/fr/malgrange/tourizm/TourizmApplication.java
@@ -0,0 +1,13 @@
+package fr.malgrange.tourizm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourizmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TourizmApplication.class, args);
+	}
+
+}
